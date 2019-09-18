c0331f4bb9ad27746ebd509957bcbb9de079dfbb
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-18 10:36:20 -0300

    criacao do projeto

---
+++ src/main/java/com/jamil/jpateste/JpatesteApplication.java
@@ -0,0 +1,13 @@
+package com.jamil.jpateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpatesteApplication.class, args);
+	}
+
+}
