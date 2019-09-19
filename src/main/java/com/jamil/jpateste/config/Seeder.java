dcf5526cf31efb13bf75d11044c16e28bef184ca
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-19 09:13:50 -0300

    melhoria no seeder ai add o deleteAll

--- src/main/java/com/jamil/jpateste/config/Seeder.java
+++ src/main/java/com/jamil/jpateste/config/Seeder.java
@@ -17,7 +17,7 @@ public class Seeder implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		repo.deleteAll();
 		User u1 = new User(null, "Jamil", "jamil@gmail.com", "987654378", "senha");
 		User u2 = new User(null, "Josue", "josue@gmail.com", "987654378", "senha1");
 		
