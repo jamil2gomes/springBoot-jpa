f1e4bf2e7e39b70e5d5c7d598ff088854cce75db
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-19 11:20:54 -0300

    atualizacao do pedido com o status pedido

--- src/main/java/com/jamil/jpateste/config/Seeder.java
+++ src/main/java/com/jamil/jpateste/config/Seeder.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jamil.jpateste.domain.Order;
 import com.jamil.jpateste.domain.User;
+import com.jamil.jpateste.domain.enuns.OrderStatus;
 import com.jamil.jpateste.repositories.OrderRepository;
 import com.jamil.jpateste.repositories.UserRepository;
 
@@ -22,20 +23,19 @@ public class Seeder implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		repo.deleteAll();
 		orderRepo.deleteAll();
 	
-		
-		
+			
 		User u1 = new User(null, "Jamil", "jamil@gmail.com", "987654378", "senha");
 		User u2 = new User(null, "Josue", "josue@gmail.com", "987654378", "senha1");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.PAID, u1);
 		
 		repo.saveAll(Arrays.asList(u1, u2));
-		
 		orderRepo.saveAll(Arrays.asList(o1,o2,o3));
 		
 	}
