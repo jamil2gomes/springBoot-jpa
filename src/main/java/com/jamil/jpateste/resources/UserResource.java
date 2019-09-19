c3d6fca6e40faae86f96d218c89b119979050350
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-19 09:14:52 -0300

    metodos findAll e findById

--- src/main/java/com/jamil/jpateste/resources/UserResource.java
+++ src/main/java/com/jamil/jpateste/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.jamil.jpateste.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jamil.jpateste.domain.User;
+import com.jamil.jpateste.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User>findAll(){
-		User u = new User(1L, "Jamil", "jamil@gmail.com", "987654378", "senha");
+	public ResponseEntity<List<User>>findAll(){
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User>findById(@PathVariable Long id){
+		User u = service.findById(id);
 		
 		return ResponseEntity.ok().body(u);
 	}
